ab5491713262d2f157f5d57e52f390d0390bf292
Robin Wenzel <info@robin-wenzel.de>
2020-05-24 10:55:51 +0200

    Add variable DB_FILE

--- mac.sh
+++ mac.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Check for args
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    echo "Usage: mac <src> <target>"
+    echo "ENV:"
+    echo "    MAC_DB_FILE << Specifies the database file location"
+    echo "                   defaults to 'mac.db'"
+    exit 1
+fi
+
 src="${1}"
 target="${2}"
-# TODO: Check if arguments actually given or empty
 
-sqlite3 mac.db """
+if [ -z "${MAC_DB_FILE}" ]
+  then
+    MAC_DB_FILE="mac.db"
+fi
+
+
+sqlite3 ${MAC_DB_FILE} """
     CREATE TABLE IF NOT EXISTS files (
         fname string primary key unique,
         fhash string
@@ -12,17 +27,17 @@ sqlite3 mac.db """
     """
 for f in $(find "${src}" -mindepth 1 -maxdepth 1 | sort); do
     fname="$(basename "${f}")"
-    exists=$(sqlite3 mac.db """
+    exists=$(sqlite3 ${MAC_DB_FILE} """
         SELECT * from files WHERE fname = '${fname}';
         """)
     if test -z ${exists}; then
         echo "Copy: ${fname}"
         rclone -q copyto "${f}" "${target}/${fname}" && \
-        sqlite3 mac.db """
-        INSERT INTO files (fname) VALUES ('$fname');
+        sqlite3 ${MAC_DB_FILE} """
+        INSERT INTO files (fname) VALUES ('${fname}');
         """ && \
-        echo "$fname has been added to the database"
+        echo "${fname} has been added to the database"
     else
-        echo "$fname in database, skipping."
+        echo "${fname} in database, skipping."
     fi
 done;
